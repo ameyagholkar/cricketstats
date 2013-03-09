5faacecc7fa975a1727ac21d6fcf757b1d280f1d
Ameya <ameya.moses@gmail.com>
2013-03-09 14:34:53 -0500

    PROD version of the connect file for appfog deployment.

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,18 @@
 <?php
-$link = mysql_connect('127.0.0.1','root',''); 
+// $link = mysql_connect('127.0.0.1','root',''); 
 
-if (!$link) { 
-	die('Could not connect to MySQL: ' . mysql_error()); 
-} 
-mysql_selectdb('Cricket', $link) or die('Error connecting to Database');
+// if (!$link) { 
+// 	die('Could not connect to MySQL: ' . mysql_error()); 
+// } 
+// mysql_selectdb('Cricket', $link) or die('Error connecting to Database');
+
+$services_json = json_decode(getenv("VCAP_SERVICES"),true);
+$mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+$username = $mysql_config["username"];
+$password = $mysql_config["password"];
+$hostname = $mysql_config["hostname"];
+$port = $mysql_config["port"];
+$db = $mysql_config["name"];
+$link = mysql_connect("$hostname:$port", $username, $password);
+$db_selected = mysql_select_db($db, $link);
 ?>
